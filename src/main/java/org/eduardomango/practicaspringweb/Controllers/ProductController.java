4e362d2ed5b33b32c989f29f97f5fe17c0131f6f
technofriki <jack_mdq97@hotmail.com>
2026-04-03 23:23:13 -0300

    Segui algo en la laptop

--- src/main/java/org/eduardomango/practicaspringweb/Controllers/ProductController.java
+++ src/main/java/org/eduardomango/practicaspringweb/Controllers/ProductController.java
@@ -3,6 +3,8 @@ package org.eduardomango.practicaspringweb.Controllers;
 import org.eduardomango.practicaspringweb.model.entities.ProductEntity;
 import org.eduardomango.practicaspringweb.model.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -16,28 +18,47 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
-    public List<ProductEntity> getAllProducts(){return productService.findAll();}
+    public ResponseEntity<List<ProductEntity>> getAllProducts(){
+        List<ProductEntity> products = productService.findAll();
+        return ResponseEntity.ok(products);}
 
 @GetMapping("/{id}")
-    public ProductEntity getProductById (@PathVariable Long id){
-        return productService.findById(id);
+    public ResponseEntity<ProductEntity> getProductById (@PathVariable Long id){
+        try{
+            ProductEntity product = productService.findById(id);
+            return ResponseEntity.ok(product);
+        } catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
 }
 
 @PostMapping
-public void createProduct (@RequestBody ProductEntity product){
+public ResponseEntity<ProductEntity> createProduct (@RequestBody ProductEntity product){
         productService.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
 }
 
 @PutMapping("/{id}")
-    public void updateProduct (@PathVariable Long id, @RequestBody ProductEntity product){
-        product.setId(id);
-        productService.update(product);
+    public ResponseEntity<ProductEntity> updateProduct (@PathVariable Long id, @RequestBody ProductEntity product){
+        try{
+            product.setId(id);
+            productService.save(product);
+            return ResponseEntity.ok(product);
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
 }
 
 @DeleteMapping("/{id}")
-    public void deleteProduct (@PathVariable Long id){
-        ProductEntity product = productService.findById(id);
-        productService.delete(product);
+    public ResponseEntity<Void> deleteProduct (@PathVariable Long id){
+        try{
+            ProductEntity product = productService.findById(id);
+            productService.delete(product);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
 }
 
 }
--- src/main/java/org/eduardomango/practicaspringweb/Controllers/UserController.java
+++ src/main/java/org/eduardomango/practicaspringweb/Controllers/UserController.java
@@ -3,6 +3,8 @@ package org.eduardomango.practicaspringweb.Controllers;
 import org.eduardomango.practicaspringweb.model.entities.UserEntity;
 import org.eduardomango.practicaspringweb.model.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,30 +16,50 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public List<UserEntity> getAllUsers(){
-        return userService.findAll();
+    public ResponseEntity<List<UserEntity>> getAllUsers(){
+
+        List<UserEntity> user = userService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @GetMapping("/{id}")
-    public UserEntity getUserById(@PathVariable Long id){
-        return userService.findById(id);
+    public ResponseEntity <UserEntity> getUserById(@PathVariable Long id){
+
+        try{
+            UserEntity user =  userService.findById(id);
+        return ResponseEntity.ok(user);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
     }
 
     @PostMapping
-    public void createUser (@RequestBody UserEntity user){
+    public ResponseEntity<UserEntity> createUser (@RequestBody UserEntity user){
+
         userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
     @PutMapping("/{id}")
-    public void updateUser (@PathVariable Long id, @RequestBody UserEntity user){
-        user.setId(id);
-        userService.update(user);
-
+    public ResponseEntity<UserEntity> updateUser (@PathVariable Long id, @RequestBody UserEntity user){
+        try {
+            user.setId(id);
+            userService.update(user);
+            return ResponseEntity.ok(user);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
 @DeleteMapping("/{id}")
-    public void deleteUser (@PathVariable Long id){
-        UserEntity user = userService.findById(id);
-        userService.delete(user);
+    public ResponseEntity<Void> deleteUser (@PathVariable Long id){
+        try {
+            UserEntity user = userService.findById(id);
+            userService.delete(user);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
 }
 }
