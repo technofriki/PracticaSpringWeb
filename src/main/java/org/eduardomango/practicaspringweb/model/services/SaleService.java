195a6897a609fe7be3f7f1751b1bdbf7e014d1c5
Juan Manuel Mariscal <jack_mdq97@hotmail.com>
2026-04-15 22:57:30 -0300

    Termine Mappers y Services

--- src/main/java/org/eduardomango/practicaspringweb/model/services/SaleService.java
+++ src/main/java/org/eduardomango/practicaspringweb/model/services/SaleService.java
@@ -1,15 +1,18 @@
 package org.eduardomango.practicaspringweb.model.services;
 
+import org.eduardomango.practicaspringweb.DTO.ProductResponse;
 import org.eduardomango.practicaspringweb.DTO.SaleRequest;
 import org.eduardomango.practicaspringweb.DTO.SaleResponse;
+import org.eduardomango.practicaspringweb.DTO.UserResponse;
+import org.eduardomango.practicaspringweb.Mapper.ProductResponseMapper;
 import org.eduardomango.practicaspringweb.Mapper.SaleRequestMapper;
 import org.eduardomango.practicaspringweb.Mapper.SaleResponseMapper;
+import org.eduardomango.practicaspringweb.Mapper.UserResponseMapper;
 import org.eduardomango.practicaspringweb.model.entities.ProductEntity;
 import org.eduardomango.practicaspringweb.model.entities.SaleEntity;
 import org.eduardomango.practicaspringweb.model.entities.UserEntity;
 import org.eduardomango.practicaspringweb.model.exceptions.SaleNotFoundException;
 import org.eduardomango.practicaspringweb.model.repositories.IRepository;
-import org.eduardomango.practicaspringweb.model.repositories.SaleRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -23,13 +26,17 @@ public class SaleService {
     private final UserService userService;
     private final SaleResponseMapper saleResponseMapper;
     private final SaleRequestMapper saleRequestMapper;
+    private final ProductResponseMapper productResponseMapper;
+    private final UserResponseMapper userResponseMapper;
 
-    public SaleService(IRepository<SaleEntity> saleRepository, ProductService productService, UserService userService, SaleResponseMapper saleResponseMapper, SaleRequestMapper saleRequestMapper) {
+    public SaleService(IRepository<SaleEntity> saleRepository, ProductService productService, UserService userService, SaleResponseMapper saleResponseMapper, SaleRequestMapper saleRequestMapper, ProductResponseMapper productResponseMapper, UserResponseMapper userResponseMapper) {
         this.saleRepository = saleRepository;
         this.productService = productService;
         this.userService = userService;
         this.saleResponseMapper = saleResponseMapper;
         this.saleRequestMapper = saleRequestMapper;
+        this.productResponseMapper = productResponseMapper;
+        this.userResponseMapper = userResponseMapper;
     }
 
     public List<SaleResponse> findAll() {
@@ -47,18 +54,22 @@ public class SaleService {
         return saleResponseMapper.convertToDto(sale);
     }
 
-    public SaleEntity registerSale(SaleRequest saleRequest) {
-        ProductEntity product = productService.findById(saleRequest.getId_product());
-        UserEntity user = userService.findById(saleRequest.getId_client());
+    public SaleResponse registerSale(SaleRequest saleRequest) {
 
-        SaleEntity sale = new SaleEntity();
+        ProductResponse productResponse = productService.findById(saleRequest.getId_product());
+        ProductEntity product = productResponseMapper.convertToEntity(productResponse);
+        UserResponse userResponse = userService.findById(saleRequest.getId_client());
+        UserEntity user = userResponseMapper.convertToEntity(userResponse);
+
+        SaleEntity sale = new  SaleEntity();
         sale.setId(System.currentTimeMillis());
         sale.setProducts(product);
         sale.setQuantity(saleRequest.getQuantity());
         sale.setClient(user);
         sale.setSaleDate(LocalDate.now());
+
         saleRepository.save(sale);
-        return new SaleResponse(sale);
+        return saleResponseMapper.convertToDto(sale);
     }
 
     public void delete(Long id) {
@@ -66,22 +77,16 @@ public class SaleService {
                 .stream()
                 .filter(s -> s.getId() == id)
                 .findFirst()
-                .orElseThrow(() -> new SaleNotFoundException("Sale not found with id:" + id));
+                .orElseThrow(SaleNotFoundException::new);
 
         saleRepository.delete(sale);
     }
 
-    public void update(Long id, SaleRequest saleRequest) {
-        SaleEntity existingSale = saleRepository.findAll()
-                .stream()
-                .filter(s -> s.getId() == id)
-                .findFirst()
-                .orElseThrow(() -> new SaleNotFoundException("Sale not found with id:" + id));
-        existingSale.setProducts(productService.findById(saleRequest.getId_product()));
-        existingSale.setClient(userService.findById(saleRequest.getId_client()));
-        existingSale.setQuantity(saleRequest.getQuantity());
-        saleRepository.update(existingSale);
-        /// NO ENTENDI NADA EN LOS SETEOS
+    public void update(Long id, SaleRequest s) {
+        findById(id);
+        SaleEntity sale = saleRequestMapper.convertToEntity(s);
+        sale.setId(id);
+        saleRepository.save(sale);
     }
 
 
